28eaf162c920cbf9584a043e37d8e66e82bf9b45
ad.osipov <ad.osipov@tensor.ru>
2022-02-03 15:32:11 +0500

    Задание по массивам

--- src/Arrays.js
+++ src/Arrays.js
@@ -3,18 +3,32 @@
   Преобразуте строку в массив чисел, отсортируйте их по возрастанию и верните строку с упорядоченными числами,
   разделенными запятыми.
 */
-export function arraySorting(data) {}
+export function arraySorting(data) {
+    let splitStr = data.split(',');
+    splitStr.map((numStr) => parseInt(numStr));
+    splitStr.sort((a, b) => a - b);
+    return Object.values(splitStr).toString();
+}
 
 /*
   В функцию arrayFiltering() приходит массив целых чисел.
   Преобразуйте массив таким образом, чтобы все элементы массива, в которых содержатся числа более 100,
   были бы удалены из массива, и верните его в качестве результата функции.
 */
-export function arrayFiltering(data) {}
+export function arrayFiltering(data) {
+    return data.filter((el) => el < 100);
+}
 
 /*
   В функцию arrayPushing() приходят два массива целых чисел размерностью 5 элементов.
   Получите третий массив размерностью 10 элементов, в котором последовательно чередуются
   значения из первых двух массивов, и верните его в качестве результата функции.
 */
-export function arrayPushing(array1, array2) {}
+export function arrayPushing(array1, array2) {
+    let unionArr = [];
+    while (array1.length > 0 && array2.length > 0) {
+        unionArr.push(array1.shift(), array2.shift());
+    }
+    unionArr.push(...array1, ...array2);
+    return unionArr;
+}
